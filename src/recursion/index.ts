e6e719fb3de6423d2241bbec3e01708a343d28ec
Shivangam Soni <shivangamsoni99@gmail.com>
2023-01-04 16:31:49 +0530

    Recursion: Project Merge Sort

--- src/recursion/index.ts
+++ src/recursion/index.ts
@@ -1,4 +1,5 @@
 import { fibs, fibsRec } from "./Fibonacci";
+import { mergeSort } from "./merge-sort";
 
 function main() {
     console.log("Fibs Loop");
@@ -6,6 +7,11 @@ function main() {
 
     console.log("Fibs Recursion");
     console.log(fibsRec(8));
+
+    const arr = [1, 3, 4, 5, -10, 2, -1, 0, -34, 5];
+    const sortedArr = mergeSort(arr);
+    console.log("Merge Sort");
+    console.log({ arr, sortedArr });
 }
 
 export default main;
